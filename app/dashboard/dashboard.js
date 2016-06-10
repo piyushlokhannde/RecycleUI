b94f8f8d998eba3a88c8b7703eab71752724d2a5
piyushlokhannde <piyush.lokhande1@gmail.com>
2016-06-11 01:14:45 +0530

    dashboard individual

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -32,20 +32,107 @@ dashboardapp.controller('dashboardCtrl', function($scope) {
 
              $scope.exampleData1 = [
                  {
-                    "key": "Series 1",
-                     "values": [ [ 1 ,122] , [ 0 , 10] ]
+                    "key": "TeamFour",
+                     "values": [  [ 1 , 10] ,[ 2 , 10], [ 3, 10]]
                  },
                  {
-                     "key": "Series 2",
-                     "values": [ [ 1 , 12] , [ 0 , 11] , [ 0 , 8] ]
+                     "key": "TeamTwo ",
+                     "values": [ [ 1 , 11] , [ 2 , 8]  , [ 3 , 8]]
                  },
                  {
-                     "key": "Series 3",
-                     "values": [ [ 1 , 13] , [ 0 , 12] ]
+                     "key": "TeamThree ",
+                     "values": [  [ 1 , 12],[ 2 , 12],[ 3 , 12] ]
                 },
                  {
-                     "key": "Series 4",
-                     "values": [ [ 1 ,12] , [ 0 , 3] ]
+                     "key": "TeamOne",
+                     "values": [  [ 1 , 12],[ 2 , 12],[ 3 , 12]]
+                 }
+             ];
+
+            $scope.xAxisTickFormatFormat  = function() {
+
+                return function (d) {              
+                switch (d) {
+                    case 1:
+                        return "Delivery";
+                        break;
+                    case 2:
+                        return "Qaulity";
+                        break;
+
+                    case 3:
+                        return "Innovation";
+                        break;
+
+                }
+            }
+            };
+
+             $scope.exampleDataperf = [
+                 {
+                    "key": "TeamFour",
+                     "values": [  [ 1 , 10] ,[ 2 , 20], [ 3, 30]]
+                 },
+                 {
+                     "key": "TeamTwo ",
+                     "values": [ [ 1 , 11] , [ 2 , 19]  , [ 3 , 27]]
+                 },
+                 {
+                     "key": "TeamThree ",
+                     "values": [  [ 1 , 12],[ 2 , 24],[ 3 , 36] ]
+                },
+                 {
+                     "key": "TeamOne",
+                     "values": [  [ 1 , 12],[ 2 , 20],[ 3 , 31]]
+                 }
+             ];
+
+              $scope.exampleData4 = [
+                 {
+                    "key": "TeamMem1",
+                     "values": [[ 0 , 200] ]
+                 },
+                 {
+                     "key": "TeamMem2 ",
+                     "values": [ [ 0 , 180] ]
+                 },
+                 {
+                     "key": "TeamMem3 ",
+                     "values": [  [ 0 , 170] ]
+                },
+                 {
+                     "key": "TeamMem4",
+                     "values": [  [ 0 , 165]]
+                 }
+                 ,
+                 {
+                     "key": "TeamMem5",
+                     "values": [  [ 0 , 160]]
+                 }
+                 ,
+                 {
+                     "key": "TeamMem6",
+                     "values": [  [ 0 , 150]]
+                 }
+                 ,
+                 {
+                     "key": "TeamMem7",
+                     "values": [  [ 0 , 135]]
+                 }
+                 ,
+                 {
+                     "key": "TeamMem8",
+                     "values": [  [ 0 , 125]]
+                 }
+                 ,
+                 {
+                     "key": "TeamMem9",
+                     "values": [  [ 0 , 120]]
+                 }
+                 ,
+                 {
+                     "key": "TeamMem10",
+                     "values": [  [ 0 , 110]]
                  }
              ];
 
