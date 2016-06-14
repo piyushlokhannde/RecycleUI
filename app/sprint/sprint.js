c23010c5005a87c3ec69364eb563ad1652e58577
Lokhande, Piyush <Piyush.Lokhande@fisglobal.com>
2016-06-14 21:43:59 +0530

    sprint changes

--- app/sprint/sprint.js
+++ app/sprint/sprint.js
@@ -9,6 +9,40 @@ angular.module('myApp.sprint', ['ngRoute'])
   });
 }])
 
-.controller('sprintCtrl', [function() {
+.controller('sprintCtrl',function($scope ) {
 
-}]);
+	var seleectdGoal=-1;
+
+	var goal  = {"name":"Goal1"};
+
+	var goalarray = new Array();
+	goalarray.push(goal);
+
+	$scope.goals = goalarray;
+
+$scope.addGoal = function () {
+
+	var des = new Object();
+	des.name ="gola3";
+	alert($scope.goals.push(des));
+}
+
+$scope.removeGoal = function () {
+	alert(seleectdGoal);
+	if(seleectdGoal > 0) {
+		goalarray.splice(seleectdGoal, 1);
+		seleectdGoal =-1;
+
+	} else if (seleectdGoal == 0 && goalarray.length ==1) {
+		goalarray.length = 0;
+		seleectdGoal =-1;
+	}
+
+	
+}
+
+$scope.setgroup = function(sgoal) {	
+	seleectdGoal = sgoal;
+} 
+
+});
