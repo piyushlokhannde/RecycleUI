a3f24f9d8322896a7e3a8c19f6a79947e4907284
piyushlokhannde <piyush.lokhande1@gmail.com>
2016-06-15 07:29:15 +0530

    sprint changes

--- app/sprint/sprint.js
+++ app/sprint/sprint.js
@@ -12,8 +12,10 @@ angular.module('myApp.sprint', ['ngRoute'])
 .controller('sprintCtrl',function($scope ) {
 
 	var seleectdGoal=-1;
+	var parmeterGoal =-1;
+    var selParam =-1;
 
-	var goal  = {"name":"Goal1"};
+	var goal  = {"name":"Goal1" ,"parameters" :[{"name":"parameter"}]};
 
 	var goalarray = new Array();
 	goalarray.push(goal);
@@ -24,25 +26,59 @@ $scope.addGoal = function () {
 
 	var des = new Object();
 	des.name ="gola3";
-	alert($scope.goals.push(des));
+	des.parameters = new Array();
+	$scope.goals.push(des);
 }
 
 $scope.removeGoal = function () {
-	alert(seleectdGoal);
+	
 	if(seleectdGoal > 0) {
 		goalarray.splice(seleectdGoal, 1);
 		seleectdGoal =-1;
 
-	} else if (seleectdGoal == 0 && goalarray.length ==1) {
-		goalarray.length = 0;
+	} else if (seleectdGoal == 0 ) {
+		goalarray.shift();
 		seleectdGoal =-1;
-	}
+	} 
 
 	
 }
 
 $scope.setgroup = function(sgoal) {	
+
 	seleectdGoal = sgoal;
 } 
+
+$scope.setparameter = function(groupIndex , paramIndex) {
+
+	parmeterGoal = groupIndex;
+	selParam = paramIndex	
+}
+
+
+$scope.addParam = function() {
+
+	var param = new Object();
+	param.name ="Param1";
+	
+	$scope.goals[seleectdGoal].parameters.push(param);
+
+
+}
+
+$scope.removeParam = function() {
+	if(selParam > 0) {
+		$scope.goals[seleectdGoal].parameters.splice(selParam, 1);
+		
+
+	} else if (selParam == 0 ) {
+		$scope.goals[seleectdGoal].parameters.shift();
+		
+	} 
+
+	parmeterGoal =-1;
+    selParam =-1;
+	
+}
 
 });
