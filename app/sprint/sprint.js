e0575dc7cd535dfe006f8bcb855c8534a4e25420
piyush.lokhande <piyush.lokhande>
2016-06-17 03:19:17 +0530

    goal changes

--- app/sprint/sprint.js
+++ app/sprint/sprint.js
@@ -5,16 +5,30 @@ var sprintapp = angular.module('myApp.sprint', ['ngRoute', 'ui.bootstrap', 'ngRe
 
 
 sprintapp.factory('MasterGoalService', function ($resource) {
-    return $resource('http://10.253.191.32:8080/getMasterGoals');
-})
+    return $resource('http://10.253.191.49:8080/getMasterGoals');
+});
+
+sprintapp.factory('MasterParamService', function ($resource) {
+    return $resource('http://10.253.191.49:8080/getSprintParameters');
+});
+
+sprintapp.factory('MasterTeamService', function ($resource) {
+    return $resource('http://10.253.191.49:8080/getTeams');
+});
+
+sprintapp.factory('MasterTeamMemberService', function ($resource) {
+    return $resource('http://10.253.191.49:8080/getTeamMembers');
+});
+
+
 
 
 function caluclateTotal(arrayObject) {
 	var total = 0;
 	angular.forEach(arrayObject, function(value, key){
 		var valueToAdd =0;
-		if(isNaN(value.percent) == false) {
-				valueToAdd = value.percent;
+		if(isNaN(value.weightage) == false) {
+				valueToAdd = value.weightage;
 		} 
 		total = total + Number(valueToAdd);
 	});
@@ -22,8 +36,16 @@ function caluclateTotal(arrayObject) {
 	return total;
 }
 
+ function updateParameterWeightage(sprintParameters) {		
+ 	var weightPerParam = (100 / sprintParameters.length );
+ 	angular.forEach(sprintParameters, function(value, key){ 		
+ 		value.weightage = weightPerParam;		
+ 	});
+ }
 
-sprintapp.controller('DialogParamCtrl', function($scope, $uibModalInstance, paramarray, $log, goalListModel) {
+
+sprintapp.controller('DialogParamCtrl', function($scope, $uibModalInstance, paramarray, $log, MasterParamService) {
+
 
 	$scope.paramList = paramarray;	
 	$scope.paramTotal = caluclateTotal(paramarray);
@@ -33,13 +55,11 @@ sprintapp.controller('DialogParamCtrl', function($scope, $uibModalInstance, para
 	}
 
 
-  	$scope.actions = [
-    	"Action Param", "Another Action Param", "Something else here Param"
-  	];
+  	$scope.actions = MasterParamService.query();
   
 	$scope.change = function(index){
-		var param = goalListModel.createNewParameter()
-		param.name = $scope.actions[index];		
+		
+		var param = $scope.actions[index];		
   		$scope.paramList.push(param); 
   		if(index > 0) {  			
   			$scope.actions.splice(index, 1);  			
@@ -60,28 +80,28 @@ sprintapp.controller('DialogParamCtrl', function($scope, $uibModalInstance, para
 
 
 
-sprintapp.controller('DialogInstCtrl', function($scope, $uibModalInstance, goalarray, $log, goalListModel) {
+sprintapp.controller('DialogInstCtrl', function($scope, $uibModalInstance, goalarray, $log, MasterGoalService) {
 
 	$scope.goalList = goalarray;
 	$scope.goalTotal = caluclateTotal(goalarray);
 
+	 
+
+
+
 	$scope.calTotalPer = function () {	
-		$scope.goalTotal = caluclateTotal($scope.goalList );
+		$scope.goalTotal = caluclateTotal($scope.goalList);
 	}
 
 
-  	$scope.actions = [
-    	"Action", "Another Action", "Something else here"
-  	];
+	$scope.actions = MasterGoalService.query();
+  	
   
-	$scope.change = function(index){
-		alert("ddd");
-		
-		var goal = goalListModel.createNewGoal();
-		goal.parameters.push(goalListModel.createNewParameter());
-		goal.name = $scope.actions[index];
-		goal.percent =0;
+	$scope.change = function(index){		
+		var goal= $scope.actions[index];		
   		$scope.goalList.push(goal); 
+  		$scope.goalTotal = caluclateTotal($scope.goalList);
+  		updateParameterWeightage(goal.sprintParameters);
   		if(index > 0) {  			
   			$scope.actions.splice(index, 1);  			
 		} else if (index == 0 ) {
@@ -112,29 +132,47 @@ sprintapp.config(['$routeProvider', function($routeProvider) {
   });
 }])
 
-.controller('sprintCtrl',function($scope, $uibModal, goalListModel) {
+sprintapp.controller('sprintCtrl',function($scope, $uibModal, MasterTeamService, MasterTeamMemberService) {
 
+	var seleectdGoal=-1;
+	var parmeterGoal =-1;
+    var selParam =-1;
+
+   $scope.teams = MasterTeamService.query();
+  	$scope.teamMemDropDown = MasterTeamMemberService.query();
+   //alert(teams);
 	
-	var goal  = goalListModel.createNewGoal();
-	goalListModel.addNewGoal(goal);
-	$scope.goals = goalListModel.goals;
+	$scope.goals = new Array();
 
 
 	$scope.removeGoal = function () {	
-		goalListModel.removeGoal();
+			if(seleectdGoal > 0) {
+			$scope.goals.splice(seleectdGoal, 1);
+			seleectdGoal =-1;
+		} else if (seleectdGoal == 0 ) {
+			$scope.goals.shift();
+			seleectdGoal =-1;
+		}
 	}
 
-	$scope.setgoal = function(sgoal) {			
-		goalListModel.selectedGoal(sgoal);
+	$scope.setgoal = function(sgoal) {	
+		seleectdGoal = sgoal;
 	} 
 
-	$scope.setparameter = function(groupIndex , paramIndex) {
-		goalListModel.setparameter(groupIndex , paramIndex);	
+	$scope.setparameter = function(groupIndex , paramIndex) {		
+		parmeterGoal = groupIndex;
+		selParam = paramIndex		
 	}
 
 
 	$scope.removeParam = function() {
-		goalListModel.removeParameter();
+		if(selParam > 0) {
+			$scope.goals[seleectdGoal].sprintParameters.splice(selParam, 1);
+		} else if (selParam == 0 ) {
+			$scope.goals[seleectdGoal].sprintParameters.shift();		
+		}
+		parmeterGoal =-1;
+    	selParam =-1;
 	
 	}
 
@@ -170,7 +208,7 @@ sprintapp.config(['$routeProvider', function($routeProvider) {
     		size: '',
     		resolve: {
     			paramarray: function () {
-    				  return goal.parameters;
+    				  return goal.sprintParameters;
     			}
     		}
 		});
@@ -181,49 +219,88 @@ sprintapp.config(['$routeProvider', function($routeProvider) {
 			});
     };
 
+
+    $scope.showParameters = function (fixedValue, paramValue) {    	
+			return (fixedValue.indexOf(paramValue.parameterType) > -1);		 
+	};
+
+
+	// Team Sectionn code  Start.
+
+	$scope.calculatePercentage = function(teamList){
+		                                
+                                var projectAvg = $scope.projectLevelAvg;
+                                //var teamList = $scope.teamMemDropDown;
+                                var sum=0;
+                                for (var i=0;i<teamList.length;i++){
+                                                sum = parseInt(sum,10) + parseInt(teamList[i].teamMemberTotal,10);
+                                }
+
+                                var teamAvg = sum/teamList.length;
+
+                                var comparedAvg = Math.round((teamAvg/projectAvg)*100)
+                                
+                                $scope.percent = comparedAvg;
+                             
+
+                                if(comparedAvg<60){
+                                                $scope.progressBarType =  "danger";
+                                } else if(comparedAvg>=60 && comparedAvg < 80) {
+                                                $scope.progressBarType = "warning";
+                                } else if (comparedAvg>=80 && comparedAvg < 90) {
+                                                $scope.progressBarType = "info";
+                                } else if (comparedAvg >=90 ) {
+                                                $scope.progressBarType = "success";
+                                }
+                               
+                };
+
+
+	$scope.addTeam = function() {
+		alert("Add Team");
+	}
+
+
+	$scope.selectTeam = function(index) {
+		$scope.selectedTeam =  $scope.teams[index];
+		$scope.teamMembers = $scope.teams[index].teamMemberTOs;
+		$scope.calculatePercentage($scope.teamMembers);	
+
+	}
+
+	$scope.removeTeamMember = function(teamMember,index) {		
+		if(index > 0) {
+			$scope.teamMembers.splice(index, 1);			
+		} else if (index == 0 ) {
+			$scope.teamMembers.shift();			
+		}
+		$scope.calculatePercentage($scope.teamMembers);	
+
+	}
+
+	$scope.selectTeamMember = function(index) {
+		$scope.teamMembers.push($scope.teamMemDropDown[index]);
+	}
+
+
+
+	// Team section code end.
+
 });
 
 
 
 
-sprintapp.service("goalListModel", function(MasterGoalService) {
+sprintapp.service("goalListModel", function() {
 
 	var seleectdGoal=-1;
 	var parmeterGoal =-1;
     var selParam =-1;
 
 	this.goals = new Array();
+	
 
-	this.createNewGoal = function() {
-		alert("www");
-
-		MasterGoalService.query(function (data) {
-			alert(data);
-
-		});
-
-    	
-		
-		var goal = new Object();
-		goal.name = "Goal1";
-		goal.percent = 0;		
-		var paramList = new Array();	
-		paramList.push(this.createNewParameter());
-		goal.parameters = paramList;
-		return goal;
-	}
-
-	this.addNewGoal = function(newGoal) {
-		this.goals.push(newGoal);
-
-	}
-
-	this.createNewParameter = function() {
-		var param1 = new Object();
-		param1.name = "Param1";
-		param1.percent =0;
-		return param1;
-	}
+	
 
 	this.removeGoal = function()  {
 		if(seleectdGoal > 0) {
@@ -236,7 +313,9 @@ sprintapp.service("goalListModel", function(MasterGoalService) {
 	}
 
 	this.selectedGoal = function(sgoal) {		
+		alert(selectedGoal);
 		seleectdGoal = sgoal;
+		alert(selectedGoal);
 	}
     
 
